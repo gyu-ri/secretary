8327cddf8e4657589dc540c585d59afe4023318e
Hyoeun Kim <gydms741@naver.com>
2021-01-10 20:44:08 +0900

    User Domain주석..올라가...

--- src/main/java/com/nj/secretary/services/user/domain/User.java
+++ src/main/java/com/nj/secretary/services/user/domain/User.java
@@ -2,7 +2,7 @@ package com.nj.secretary.services.user.domain;
 
 public class User {
     private String userName;//사용자이름, 자체로그인
-    private String userId;//사용자아이디
+    private String userId;//사용자아이디..
 
     public User(String userName,String userId){
         this.userName = userName;//주석..
