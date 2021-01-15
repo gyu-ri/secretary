f8cb885d827cafa13301a14a146ceeb6a37f345e
senbro <yygg1926@naver.com>
2021-01-15 13:22:35 +0900

    seongmin : diary emotion 완료 diary 작성 db연결진행중

--- src/main/java/com/nj/secretary/services/diary/service/DiaryService.java
+++ src/main/java/com/nj/secretary/services/diary/service/DiaryService.java
@@ -1,4 +1,14 @@
 package com.nj.secretary.services.diary.service;
 
-public class DiaryService {
+import com.nj.secretary.services.diary.domain.Diary;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface DiaryService {
+
+    public void addDiary(Diary diary);
+
+
 }
