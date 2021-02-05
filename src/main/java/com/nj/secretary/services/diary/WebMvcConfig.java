1492d806452c5eec9cca9b91fca08d068cd9c6ba
Yoon <qkzld13@gmail.com>
2021-02-05 22:15:40 +0900

    Yoon : image path(finish)

--- src/main/java/com/nj/secretary/services/diary/WebMvcConfig.java
+++ src/main/java/com/nj/secretary/services/diary/WebMvcConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
-        registry.addResourceHandler("/images/diaryImage/**").addResourceLocations("file:///C:\\Users\\clipse\\IdeaProjects\\secretary\\src\\main\\resources\\static\\images\\diaryImage");
+        String path = System.getProperty("user.dir");
+        registry.addResourceHandler("/images/**").addResourceLocations("file:///"+path+"\\src\\main\\resources\\static\\images\\");
     }
 }
